772ef08af96c2630ee50fc66ef92b84fd34b3924
Matheusbr1 <matheusbaron10@gmail.com>
2019-11-20 13:23:07 -0300

    This is a crud built with react

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido por <i className="fa fa-heart text-danger"></i>
+            <strong>Cod<span className="text-danger" >3</span>r</strong>
+        </span>
+    </footer>
---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Logo.css'
+import logo from '../../assets/imgs/React.png'
+
+export default props => 
+<aside className="logo" >
+    <a href="/">
+        <img src={logo} alt="logo"/>
+    </a>
+</aside>
